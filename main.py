0ca6a682a35c8364e97d09c076f0a5c13549bc7d
pkgzx <olvadis2004@gmail.com>
2026-02-22 20:38:21 -0500

    setup base dependencies and initial FastAPI application structure

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 """This file contains the main application entry point."""
 
+from fastapi import (
+    FastAPI,
+    Request,
+    status
+)
+
+import uvicorn
 
 
-def main():
-    print("Hello from siomi!")
+app = FastAPI(
+    title="Siomi AI",
+    version="0.0.1",
+    description="""Siomi is a platform multi-agent AI to generate answers to our clients by messages, documents, graphics, report from big data and more""",
+)
+
+@app.get("/")
+async def root(request: Request) -> str:
+    """Root probe to test api"""
+    return "Hello"
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
